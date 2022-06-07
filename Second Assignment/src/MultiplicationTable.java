ed4b461341597165b6800be26f39e363ef209bb3
supriyaa patnaik <supriyaa patnaik@LAPTOP-60P4L3HN>
2022-06-07 11:48:08 +0530

    first commit

---
+++ Second Assignment/src/MultiplicationTable.java
@@ -0,0 +1,16 @@
+public class MultiplicationTable 
+{
+		public static void main(String[] args) 
+		{			
+			{
+				int num=9;
+				for(int i=1;i<=30;++i)
+				{
+					//System.out.println(num);
+					System.out.printf("%d*%d=%d \n",num,i,num*i);
+				}
+			}
+
+		}
+
+}
